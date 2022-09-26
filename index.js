ea5b78b2dd37d7d52c997d9eb01a1d2294871f29
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2022-09-26 17:32:02 +0300

    try

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-const CHECK_ANSWERS = ['да 👍', 'нет 👎', 'похуй 😐︎', 'сериал.. 📺', 'Топор подскажет! 🪓', 'белое ⚪', 'серое 🧙‍♂️', 'чёрное ⚫'];
-const RANDOM_ANSWERS = ['Факт! 👆'];
+const CHECK_ANSWERS = [
+  "да 👍",
+  "нет 👎",
+  "похуй 😐︎",
+  "сериал.. 📺",
+  "Топор подскажет! 🪓",
+  "белое ⚪",
+  "серое 🧙‍♂️",
+  "чёрное ⚫",
+];
+const RANDOM_ANSWERS = ["Факт! 👆"];
 const RANDOM_ANSWER_CHANCE = 0.02;
 
 const token = process.env.BOT_TOKEN;
 
-const eaglesBot = new TelegramBot(token, {polling: true});
+const runBot = () => {
+  const eaglesBot = new TelegramBot(token, { polling: true });
 
-eaglesBot.onText(/\/check/, (msg, match) => {
+  eaglesBot.onText(/\/check/, (msg, match) => {
     const chatId = msg.chat.id;
 
-    const answer = CHECK_ANSWERS[Math.floor(Math.random()*CHECK_ANSWERS.length)];
+    const answer =
+      CHECK_ANSWERS[Math.floor(Math.random() * CHECK_ANSWERS.length)];
 
     eaglesBot.sendMessage(chatId, `Я считаю, ${answer}`);
-});
+  });
 
-eaglesBot.on('message', (msg) => {
+  eaglesBot.on("message", (msg) => {
     const shouldAnswer = Math.random() <= RANDOM_ANSWER_CHANCE;
     if (!shouldAnswer) {
-        return;
+      return;
     }
 
     const chatId = msg.chat.id;
-    const answer = RANDOM_ANSWERS[Math.floor(Math.random()*RANDOM_ANSWERS.length)];
+    const answer =
+      RANDOM_ANSWERS[Math.floor(Math.random() * RANDOM_ANSWERS.length)];
 
     eaglesBot.sendMessage(chatId, answer);
-});
+  });
+};
+
+runBot();
+
+setInterval(runBot, 5000000);
