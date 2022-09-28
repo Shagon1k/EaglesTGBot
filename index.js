5b0d3f4f6ad7114cb16c6145141b4f5aaf418faa
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2022-09-28 23:55:50 +0300

    Refactor and some new features

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
+const stringMath = require("string-math");
 
 const CHECK_ANSWERS = [
   "да 👍",
@@ -10,15 +11,44 @@ const CHECK_ANSWERS = [
   "серое 🧙‍♂️",
   "чёрное ⚫",
 ];
-const RANDOM_ANSWERS = ["Факт! 👆"];
-const RANDOM_ANSWER_CHANCE = 0.02;
+const RANDOM_ANSWERS = ["Факт! 👆", "Гигигага 😂"];
+const RANDOM_ANSWER_CHANCE = 0.04;
+const GAY_ANSWER_CHANCE = 0.3;
+
+const GAY_REGEXP = /гей/i;
+const INESSA_REGEXP = /инесса/i;
+const MATH_EXPRESSION_REGEXP = /([\d\(\)]+).+([\d\(\)]+)/;
 
 const token = process.env.BOT_TOKEN;
+
+const handleInessaMath = (bot, chatId, msgId, msgText) => {
+  const mathExpression = msgText.match(MATH_EXPRESSION_REGEXP)[0];
+  const mathResult = stringMath(mathExpression);
+
+  if (mathResult !== null) {
+    bot.sendMessage(chatId, `${mathResult}, мой птенчик 😘`, {
+      reply_to_message_id: msgId,
+    });
+  }
+};
+
+const handleGayRequest = (bot, chatId, msgId) => {
+  const shouldGayAnswer = Math.random() <= GAY_ANSWER_CHANCE;
+
+  if (shouldGayAnswer) {
+    bot.sendMessage(chatId, "Естественно! 👆", {
+      reply_to_message_id: msgId,
+    });
+  }
+};
 
 const runBot = () => {
   const eaglesBot = new TelegramBot(token, { polling: true });
 
-  eaglesBot.onText(/\/check/, (msg, match) => {
+  // Log TG Bot errors if any
+  eaglesBot.on("polling_error", (msg) => console.log(msg));
+
+  eaglesBot.onText(/\/check/, (msg) => {
     const chatId = msg.chat.id;
 
     const answer =
@@ -28,19 +58,29 @@ const runBot = () => {
   });
 
   eaglesBot.on("message", (msg) => {
-    const shouldAnswer = Math.random() <= RANDOM_ANSWER_CHANCE;
-    if (!shouldAnswer) {
-      return;
+    const chatId = msg.chat.id;
+    const { id: msgId, text: msgText } = msg;
+
+    switch (typeof msgText === 'string') {
+      case INESSA_REGEXP.test(msgText):
+        handleInessaMath(eaglesBot, chatId, msgId, msgText);
+        break;
+      case GAY_REGEXP.test(msgText):
+        handleGayRequest(eaglesBot, chatId, msgId);
+        break;
+      default:
+        return;
     }
 
-    const chatId = msg.chat.id;
-    const answer =
-      RANDOM_ANSWERS[Math.floor(Math.random() * RANDOM_ANSWERS.length)];
+    const shouldDefaultAnswer = Math.random() <= RANDOM_ANSWER_CHANCE;
 
-    eaglesBot.sendMessage(chatId, answer);
+    if (shouldDefaultAnswer) {
+      const answer =
+        RANDOM_ANSWERS[Math.floor(Math.random() * RANDOM_ANSWERS.length)];
+
+      eaglesBot.sendMessage(chatId, answer);
+    }
   });
 };
 
 runBot();
-
-setInterval(runBot, 5000000);
